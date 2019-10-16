df62cc364960f55f77a1f5dec5b6b56a64374784
niedan123 <2911843931@qq.com>
2019-10-16 17:07:44 +0800

    太难了

--- minpapi/app.js
+++ minpapi/app.js
@@ -3,11 +3,14 @@ const fs = require('fs');
 const funcs = require('./functions');
 const marked = require('marked');
 const mdb = require('./initdb');
+const crypto = require('crypto');
+const wxmsg = require('./ch13/src/msghandle');
+const parsexml=require ('xml2js').parseString;
 
 var app = new titbit({
   //debug: true, //开启调式模式，会输出错误信息
   //showLoadInfo: false,
-  daemon:true,
+  daemon:true
 });
 
 var {router} = app; //相当于 var router = app.router;
@@ -66,10 +69,35 @@ router.get('/images/:name', async c => {
   }
 });
 
-router.get('/a',async c =>{
+router.get('/a',async c=>{
   //返回URL查询参数
-
-  c.res.body = c.query;
+  c.res.body=c.query;
 });
+app.router.post('/wx/msg',async c=>{
+    try{
+        let data =await new Promise((rv,rj)=>{
+            parsexml(c.body,{explicitArray:false},
+            (err,result)=>{
+                if(err){rj(err);}
+                else{rv(result.xml);}
+            });
+        });
+        let retmsg={
+            touser:data.FromUserName,
+            fromuser:data.ToUserName,
+            msgtype:'',//为空，在处理
+            msgtime:parseInt(Date.now()/1000),
+            msg:data.Content
+        };
+        //交给信息派发函数处理
+        //把解析的消息和要返回的数据对象传递下去
+        c.res.body=wxmsg.msgDispatch(data,retmsg);
+
+    }catch(err){
+        console.log(err);
+    }
+});
+
+
 
 app.daemon(8000, 'localhost');
